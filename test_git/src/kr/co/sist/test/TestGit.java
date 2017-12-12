23e52459a04f77db54530a64f07b826fb80a4dcc
user <user@user-PC>
2017-12-12 14:06:57 +0900

    51515

--- test_git/src/kr/co/sist/test/TestGit.java
+++ test_git/src/kr/co/sist/test/TestGit.java
@@ -5,6 +5,10 @@ public class TestGit {
 	public static void main(String[] args) {
 		int i=10;
 		System.out.println("Git Hub 연습"+i);
+		System.out.println("수정수정");
+		
+	//	ffffff 
+		//ffff
+		//dd
 	}
-
 }
