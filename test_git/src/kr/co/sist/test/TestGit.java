acf734926a7507fb1d3915cef3e0eed4bfda9a04
user <user@user-PC>
2017-12-12 12:14:18 +0900

    두번째 수정

--- test_git/src/kr/co/sist/test/TestGit.java
+++ test_git/src/kr/co/sist/test/TestGit.java
@@ -3,7 +3,8 @@ package kr.co.sist.test;
 public class TestGit {
 
 	public static void main(String[] args) {
-		System.out.println("Git Hub 연습");
+		int i=10;
+		System.out.println("Git Hub 연습"+i);
 	}
 
 }
